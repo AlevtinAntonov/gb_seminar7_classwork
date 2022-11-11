9764717754505032a8b3164e266b96a7fb9c6fb8
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-11-11 21:33:56 +0300

    controller, main

--- controller.py
+++ controller.py
@@ -1,1 +1,13 @@
 # User interface, соединяет пользователя, фронт и бэк
+
+import view
+import model
+
+
+def run():
+    while True:
+        input('Вычитаем, введите А: ', a)
+        input('Вычитаем, введите Б: ', b)
+        result = model.divide(a, b)
+        view.show(a, b, result)
+        logger.calculation_logger(a, b, result)
